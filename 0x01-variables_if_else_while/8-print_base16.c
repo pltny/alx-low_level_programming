7a79311d795ac7d93badff132a49571feeec2d95
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-13 07:05:16 -0800

    printing hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * mian - Entry point
+ *
+ * Description: printing hexadecimal
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int m;
+	char n;
+ /* exercutes hexadeciaaml*/
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
