22e5a032bb31c83f8c7ed0a5222d2d8bc893c028
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-13 05:30:51 -0800

    printing numbers 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description - printing numbers 0 to 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	if (n <= 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
