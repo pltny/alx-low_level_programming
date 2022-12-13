3ddd1329cac45ac58453e37d2b963afc239e553e
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-13 07:23:37 -0800

    printing numbers 0 to 10 in ascending order with , and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * min - entry point
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
