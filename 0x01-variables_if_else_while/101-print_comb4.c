05b42b35f858a4d808718c78fc5b0e7793d452a5
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-13 13:38:49 -0800

    printing tens,hundred and ones

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,19 +17,18 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) ||
-(tens > ones) || (hundreds > tens)))
-			        {
-    				        putchar(hundreds);
+				if (!((ones == tens) || (tens == hundreds) || 
+							(tens > ones) || (hundreds > tens)))
+				{
+					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' &&
-tens == '8'))
+					if (!(ones = '9' && hundreds == '7' && tens == '8'))
 					{
 						putchar(',');
 						putchar(' ');
 					}
-			        }
+				}
 			}
 		}
 	}
