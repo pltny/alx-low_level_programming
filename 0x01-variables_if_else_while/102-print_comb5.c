2f7e678931f8edf34fbaeb4ebdcd159dfe3f8d83
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-15 14:59:10 -0800

    commited

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,21 +14,21 @@ int main(void)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			if (i < j)	
-		        {
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-		                putchar(' ');
-	                        putchar((j / 10) + 48);
-		                putchar((j % 10) + 48);
-		                if (i !=98 || j != 99)
-			        {
-			                putchar(',');
-			                putchar(' ');
+			if (i < j)
+				{
+				        putchar((i / 10) + 48);
+				        putchar((i % 10) + 48);
+		                        putchar(' ');
+	                                putchar((j / 10) + 48);
+		                        putchar((j % 10) + 48);
+		                        if (i !=98 || j != 99)
+			                {
+			                        putchar(',');
+			                        putchar(' ');
+			                }
 			        }
-			}     
 	        }
         }
         putchar('\n');
         return (0);
-}	
+}
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,4 +38,3 @@ void print_times_table(int n)
 }
 }
 }
-}
