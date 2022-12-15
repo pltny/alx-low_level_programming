6b43add52e587d476744873a7333d537ff0decca
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-14 16:53:35 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints number
+ * @n: no to be checked
+ * Return:1 for positivenum
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else 
+	{
+	_putchar(48);
+	return (0);
+	}
+}
