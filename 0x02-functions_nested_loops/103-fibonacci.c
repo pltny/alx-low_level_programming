b94cdf3940ed810f87719bad304f9b42bd377e0e
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-15 14:32:34 -0800

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,9 +9,9 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2, sum = k;
 
-	while ( k + j < 4000000)
+	while (k + j < 4000000)
 	{
-        k += j;
+k += j;
 	if (k % 2 == 0)
 	sum += k;
 	j = k - j;
