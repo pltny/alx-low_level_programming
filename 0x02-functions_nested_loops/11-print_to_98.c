6a66bd5de3aaa6485e1e8dff2f51a42de2db2a69
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-15 12:24:49 -0800

    8 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,18 @@
 #include "main.h"
+
 #include<stdio.h>
+
 
 /**
  * print_to_98 - prints natural numbers
  * @n: starting point
  * Return: Always 0
  */
+
 void print_to_98(int n)
+
 {
+
 	if (n <= 98)
 	{
 	for (; n <= 98; n++)
@@ -35,7 +40,7 @@ printf("\n");
 break;
 }
 else
-	{
+{
 	printf("%d, ", n);
 	}
 }
