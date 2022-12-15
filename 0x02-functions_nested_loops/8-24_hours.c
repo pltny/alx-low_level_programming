e1592f3b8bfedc79599c215421985c6a73b2b479
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-15 01:44:18 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-	for (b= 0; b <= 9; b++)
+	for (b = 0; b <= 9; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 	{
-	for (c = 0; c <=5; c++)
+	for (c = 0; c <= 5; c++)
 	{
 	for (d = 0; d <= 9; d++)
 	{
@@ -23,6 +23,7 @@ void jack_bauer(void)
 	_putchar(58);
 	_putchar(c + '0');
 	_putchar(d + '0');
+	_putchar('\n');
 	}
 	}
 	}
