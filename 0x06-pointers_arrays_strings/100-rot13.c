bb060cbea6cd8d63cb619f38b885da7ee05013ea
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-22 14:33:45 -0800

    100-rot13.c' git push

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,13 +15,14 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; a[j] != '\0'; j++)
+		for (j = 0; c[j] != '\0'; j++)
 		{
 			if (s[i] == c[j])
 			{
-				s[i] = c[j];
+				s[i] = d[j];
 				break;
 			}
 		}
 	}
+	return (s);
 }
