f157304eecd8d19ca51a4da2d42aa1e23adbc9ee
Derick Gitonga <gitongaderick17@gmail.com>
2022-12-22 08:31:22 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - unction that concatenates two strings.
+ * @dest: destination string
+ * @src: serves as a source spring
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int zlen =  0, n;
+
+	while (dest[zlen])
+	{
+		zlen++;
+	}
+
+	for (n = 0; src[n] != 0; n++)
+	{
+		dest[zlen] = src[n];
+		zlen++;
+	}
+
+	dest[zlen] = '\n';
+	return (dest);
+}
